e1f3e63a87978a35144733df83d315652ae0e1e5
kashish-1209 <kashish.maggu@mountblue.io>
2019-08-27 14:39:44 +0530

    3 and 4th

--- src/Main.java
+++ src/Main.java
@@ -44,6 +44,34 @@ public class Main {
 			
 			System.out.println();System.out.println();System.out.println();
 			
+			br = new BufferedReader(new FileReader("/home/kashish/Desktop/IPL/deliveries.csv"));
+			
+			br.readLine();
+			ArrayList<Deliveries> stats = new ArrayList<>();
+			while((line=br.readLine())!=null) {
+				line+=", , ,";
+				String[] delivery = line.split(COMMA_DELIMITER);
+				
+				if(delivery.length>0) {
+					Deliveries d = new Deliveries(Integer.parseInt(delivery[0]), Integer.parseInt(delivery[1]), delivery[2], delivery[3], Integer.parseInt(delivery[4]),
+							Integer.parseInt(delivery[5]),delivery[6], delivery[7], delivery[8], Integer.parseInt(delivery[9]), Integer.parseInt(delivery[10]),
+							Integer.parseInt(delivery[11]), Integer.parseInt(delivery[12]), Integer.parseInt(delivery[13]), Integer.parseInt(delivery[14]),
+							Integer.parseInt(delivery[15]), Integer.parseInt(delivery[16]), Integer.parseInt(delivery[17]), delivery[18], delivery[19], delivery[20]);
+					stats.add(d);
+				}
+				
+			}
+			
+			ExtraRunsConcededperTeam(list,stats);
+			
+			System.out.println();System.out.println();System.out.println();
+			
+			topEconomicalBowler(list,stats);
+			
+		//	for(int i=0;i<stats.size();i++)
+			//	System.out.println(stats.get(i));
+			
+			
 			br.close();
 			
 		}
@@ -62,6 +90,81 @@ public class Main {
 			// TODO Auto-generated catch block
 			System.out.println(e);
 		}
+	}
+
+	private static void topEconomicalBowler(ArrayList<Matches> list, ArrayList<Deliveries> stats) {
+		// TODO Auto-generated method stub
+		int k=0,x=0;
+		HashMap<String,Integer> economicalBowler = new HashMap<>();
+		for(int i=0;i<list.size();i++) {
+			
+			if(list.get(i).getSeason()==2015) {
+				for(int j=0;j<stats.size();j++) {
+					
+					if(list.get(i).getId()==stats.get(j).getMatchId()) {
+						k++;
+						if(economicalBowler.containsKey(stats.get(j).getBowler()))
+							economicalBowler.put(stats.get(j).getBowler(), economicalBowler.get(stats.get(j).getBowler())+stats.get(j).getTotalRuns());
+						else {
+							economicalBowler.put(stats.get(j).getBowler(), stats.get(j).getTotalRuns());
+							x++;
+						}
+					}
+			
+					
+				}
+				
+			}
+			
+			
+		}
+		int max=0;
+		String str = "";
+		for (Map.Entry entry : economicalBowler.entrySet()) { 
+             
+			if(	(int)entry.getValue()>max) {
+				max = (int)entry.getValue();
+				str = (String)entry.getKey();
+			}
+			
+        }
+		
+		System.out.println(str);
+		
+	}
+
+	private static void ExtraRunsConcededperTeam(ArrayList<Matches> list, ArrayList<Deliveries> stats) {
+		// TODO Auto-generated method stub
+		//HashMap<Integer, ArrayList<Deliveries>> newStats = new HashMap<>();
+		
+		
+		
+		int k=0;
+		HashMap<String,Integer> RunsConceded = new HashMap<>();
+		for(int i=0;i<list.size();i++) {
+			
+			if(list.get(i).getSeason()==2016) {
+				for(int j=0;j<stats.size();j++) {
+					
+					if(list.get(i).getId()==stats.get(j).getMatchId()) {
+						if(RunsConceded.containsKey(stats.get(j).getBattingTeam()))
+							RunsConceded.put(stats.get(j).getBattingTeam(), RunsConceded.get(stats.get(j).getBattingTeam())+stats.get(j).getExtraRuns());
+						else
+							RunsConceded.put(stats.get(j).getBattingTeam(), stats.get(j).getExtraRuns());
+					}
+					k++;
+					
+				}
+			}
+			k++;
+			
+		}
+		
+		for (Map.Entry entry : RunsConceded.entrySet()) { 
+            System.out.println(entry.getKey() + " " + entry.getValue()); 
+        }
+		//System.out.println(k);
+		
 	}
 
 	private static void MatchesWonByEachTeam(ArrayList<Matches> list) {
